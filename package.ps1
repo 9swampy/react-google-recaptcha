d2d3c8e22d04e887f4dab33ff751fdf261c85d87
9swampy <github@prims.com>
2024-04-08 05:17:27 +0100

    Tidy the package

---
+++ package.ps1
@@ -0,0 +1,11 @@
+﻿Set-Location D:\git\react-google-recaptcha
+Remove-Item package -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item 9swampy-react-google-recaptcha-v0.0.0-beta0.tgz -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item 9swampy-react-google-recaptcha-v0.0.0-beta0.tar -Recurse -Force -ErrorAction SilentlyContinue
+npm run build
+npm pack
+& 'C:\Program Files\7-Zip\7z.exe' x 9swampy-react-google-recaptcha-v0.0.0-beta0.tgz
+& 'C:\Program Files\7-Zip\7z.exe' l 9swampy-react-google-recaptcha-v0.0.0-beta0.tar
+Remove-Item package -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item 9swampy-react-google-recaptcha-v0.0.0-beta0.tgz -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item 9swampy-react-google-recaptcha-v0.0.0-beta0.tar -Recurse -Force -ErrorAction SilentlyContinue
